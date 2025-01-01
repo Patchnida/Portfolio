b19dd22af942380b474392500141dbad93676817
Patchnida Hemwannanukul <patchnida2547@gmail.com>
2025-01-01 22:41:41 +0700

    update footer

---
+++ src/components/footer.js/footer.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <div className=" bg-white w-full h-16 shadow-t-md sticky bottom-0">
+        <div className="flex gap-4 justify-end mx-5 items-center my-0 py-2 w-auto h-full border-t-2">
+            <Link href="tel:0638102695">
+                <Image src="/phone.png" alt="" width={30} height={30}/>
+            </Link>
+            <Link target="_blank" href="https://line.me/ti/p/bEvaBkdp2e" passHref>
+                <Image src="/line.png" alt="" width={30} height={30}/>
+            </Link>
+            <Link href="mailto:patchnida.hemw@gmail.com" passHref>
+                <Image src="/email.png" alt="" width={30} height={30}/>
+            </Link>
+        </div>
+    </div>
+        
+
+  )
+}
+
+export default Footer;
